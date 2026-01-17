cf14c2782be74209afc1b699bb5a1b80d730e9c2
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2026-01-17 16:19:36 +0100

    feat(easteregg)

--- src/components/pages/pageComponent.tsx
+++ src/components/pages/pageComponent.tsx
@@ -11,6 +11,8 @@ import { useEffect, useState } from 'react'
 import { useWindow } from '@/contexts/window/window.context'
 import { useTaskbar } from '@/contexts/taskbar/taskbar.context'
 import { useMatrix } from '@/contexts/matrix/matrix.context'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 const PageComponent = () => {
     const {
@@ -50,11 +52,34 @@ const PageComponent = () => {
 
     const [ctrlPressed, setCtrlPressed] = useState<boolean>(false)
 
+    const [keysPressed, setKeysPressed] = useState<string>('')
+    const [animateEasterEgg, setAnimateEasterEgg] = useState<boolean>(false)
+
+    useEffect(() => {
+        console.log(keysPressed);
+        if (keysPressed === '33') {
+            setAnimateEasterEgg(true)
+            return
+        }
+        setAnimateEasterEgg(false)
+    }, [keysPressed])
+
     return (
         <div
             className={style.page}
             onKeyDown={(k) => {
-                console.log(k);
+                if (Number.isNaN(Number(k.key))) return
+
+                if (keysPressed?.length === 2) {
+                    if (k.key === '3' && keysPressed === '33') {
+                        setKeysPressed('3')
+                        return
+                    }
+                    setKeysPressed(prev => prev.at(prev.length - 1) + k.key)
+                    return
+                }
+
+                setKeysPressed(prev => prev + k.key)
             }}>
             <div className={style.virtualBody}/>
             <div
@@ -64,7 +89,7 @@ const PageComponent = () => {
                     resetGlobalStyle()
                     hide(true)
                     unFocusAll()
-                    unfocusTaskbarMenus()
+                    unfocusTaskbarMenus()  
                 }}
                 onContextMenu={(e) => {
                     e.preventDefault()
@@ -72,7 +97,7 @@ const PageComponent = () => {
                     setItemUuid(undefined)
                     hide(false)
                 }}
-                
+
                 onKeyDown={(k) => {
                     if (k.code == 'ControlLeft') setCtrlPressed(true)
                     if (k.code == 'KeyV' && ctrlPressed) {
@@ -91,6 +116,33 @@ const PageComponent = () => {
                 <CtxMenu xy={getXy()} itemUuid={getItemUuid()} hide={isHidden()}/>
                 <DesktopRender/>
             </div>
+            {
+                animateEasterEgg && (
+                    <motion.div
+                        initial={{ y: 500, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ 
+                            duration: 1.2, 
+                            ease: "easeOut",
+                            delay: 0.3
+                        }}
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            right: 0,
+                            zIndex: 999998,
+                            filter: 'drop-shadow(0 0 0.45rem red)'
+                        }}
+                    >
+                        <Image
+                            src={'/img/fernando.png'}
+                            alt='Easteregg'
+                            width={320}
+                            height={320}
+                        />
+                    </motion.div>
+                )
+            }
         </div>
     )
 }
